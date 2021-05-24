6ddeb8817c53a968479b3fc413e412a664d59c81
BlancaML <blancamorenodeluna@gmail.com>
2021-05-24 21:15:46 +0200

    done

--- app.js
+++ app.js
@@ -2,14 +2,30 @@ const express = require('express');
 const app = express();
 
 
-/**
- * View setup
- */
-app.set('views', __dirname + 'views');
+app.use(express.static('public'))
+app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
+
+const data = require('./data/got.json');
+
+
+app.get('/episodes', (req, res) => {
+    res.render('episodes', { 
+        data: data
+    })
+});
+
+app.get('/episodes/:season', (req, res) => {
+    const season = data.filter((e) => e.season === Number(req.params.season))
+    
+    res.render('episodes', { 
+        data: season
+    })
+});
+
 
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Ready! Listening on port ${port}`);
+    console.log(`Ready! Listeniiiiing on port ${port}`);
 });
